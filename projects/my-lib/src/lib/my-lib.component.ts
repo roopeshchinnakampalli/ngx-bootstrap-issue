0f47d3787f9c5dab840941383ce68ba727ac589e
Roopesh Chinnakampalli <rchinnakampalli@worldbankgroup.org>
2020-07-13 11:51:56 -0400

    updates

--- projects/my-lib/src/lib/my-lib.component.ts
+++ projects/my-lib/src/lib/my-lib.component.ts
@@ -3,16 +3,19 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "lib-my-lib",
   template: `
-    <ndc-dynamic [ndcDynamicComponent]="comp"></ndc-dynamic>
-    <button
-      type="button"
-      class="btn btn-default btn-secondary"
-      popover="Vivamus sagittis lacus vel augue laoreet rutrum faucibus."
-      popoverTitle="Popover on top"
-      placement="top"
-    >
-      Popover on top
-    </button>
+    <ng-container>
+      <a
+        href="javascript:;"
+        class="btn btn-default btn-secondary"
+        [popover]="test"
+        popoverTitle="Popover on top"
+        placement="top"
+        #pop3="bs-popover"
+      >
+        Popover on top
+      </a>
+    </ng-container>
+    <ng-template #test> </ng-template>
   `,
   styles: [],
 })
--- projects/my-lib/src/lib/my-lib.module.ts
+++ projects/my-lib/src/lib/my-lib.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from "@angular/core";
+import {
+  NgModule,
+  NO_ERRORS_SCHEMA,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MyLibComponent } from "./my-lib.component";
-import { DynamicModule } from "ng-dynamic-component";
 import { PopoverModule } from "ngx-bootstrap/popover";
 
 @NgModule({
   declarations: [MyLibComponent],
-  imports: [DynamicModule, PopoverModule.forRoot(), CommonModule],
+  imports: [PopoverModule.forRoot(), CommonModule],
   exports: [MyLibComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class MyLibModule {}
